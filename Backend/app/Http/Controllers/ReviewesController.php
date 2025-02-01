75c3934a6548997c7d1a619c8931afddf06c90f6
Błażej <wargun364@gmail.com>
2025-02-01 17:59:34 +0100

    creating reviewes migrate, modal, controler

---
+++ Backend/app/Http/Controllers/ReviewesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\reviewes;
+use Illuminate\Http\Request;
+
+class ReviewesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(reviewes $reviewes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(reviewes $reviewes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, reviewes $reviewes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(reviewes $reviewes)
+    {
+        //
+    }
+}
